53e531746336d382e000421092de61b72c2b6560
Kyou Hee Lee <stillqe@MacBook-Pro-Q.local>
2021-10-11 16:44:37 -0700

    adjusted for Azure

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 """Flask configuration."""
+import os
 from os import environ, path
 from dotenv import load_dotenv
 
@@ -8,8 +9,8 @@ print(basedir)
 
 class Config:
     """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY')
-    SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
+    SECRET_KEY = os.environ('SECRET_KEY')
+    SESSION_COOKIE_NAME = os.environ('SESSION_COOKIE_NAME')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -19,7 +20,7 @@ class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    DATABASE_URI = environ.get('PROD_DATABASE_URI')
+    DATABASE_URI = os.environ('PROD_DATABASE_URI')
 
 
 class DevConfig(Config):
