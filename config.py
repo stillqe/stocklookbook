6d1bf4b10ecf6e61789bb2507f0ad51f452f9835
Kyou Hee Lee <stillqe@MacBook-Pro-Q.local>
2021-10-14 12:50:21 -0700

    .

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 """Flask configuration."""
 from os import environ, path
 
-from dotenv import load_dotenv
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+# from dotenv import load_dotenv
+# basedir = path.abspath(path.dirname(__file__))
+# load_dotenv(path.join(basedir, '.env'))
+
+database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}?sslmode=require'.format(
+    dbuser=environ.get('DBUSER'),
+    dbpass=environ.get('DBPASS'),
+    dbhost=environ.get('DBHOST'),
+    dbname=environ.get('DBNAME')
+)
 
 class Config:
     """Base config."""
@@ -11,6 +18,7 @@ class Config:
     SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
+    SQLALCHEMY_DATABASE_URI = database_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -18,7 +26,6 @@ class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
 
 
 class DevConfig(Config):
@@ -26,4 +33,3 @@ class DevConfig(Config):
     DEBUG = True
     use_reloader = False
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
