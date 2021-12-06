04c6de7c170710bf74251e734e00b9e82f0eefd0
Kyou Hee Lee <stillqe@MacBook-Pro-Q.local>
2021-12-06 11:42:49 -0800

    masonry veiw applied

--- stocklookbookapp/static/script/interaction.js
+++ stocklookbookapp/static/script/interaction.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){
-    $(document).on('click','.radio', function() {
-        var period = $(this).attr('value');
-        $(this).hide();
-        req = $.ajax({
-            url: '/update/'
-            type: 'GET',
-            data: {period: period}
-        });
 
-        req.done(function(data) {
-            $('#display').fadeOut(100).fadeIn(100);
-            $('#display').html(data);
+    $(".btn").click(function(){
+
+
+        $(".btn").removeClass("active");//removes classes everywhere
+        $(this).toggleClass("active");//adds the class at the right button
+
+        // Get value from input element on the page
+        var choiceValue = $(this).attr("id");
+        var periodValue = $("#period").find("a.active").attr("data-filter");
+
+        $('.grid').load('/collection' +  ' .grid', {choice: choiceValue, period:periodValue}, function(){
+            $('.grid').imagesLoaded(function () {
+                $('.grid').masonry({
+                    itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+                    percentPosition: true
+                });
+            });
         });
     });
+
+    $("#period").find('a').click(function() {
+        var $period = $("#period");
+        $period.find("a").removeClass("active");
+        $(this).addClass("active");
+
+        var choiceValue = $("#nav-button").find("button.active").attr("id");
+        var periodValue = $(this).attr("data-filter");
+
+        $('.grid').load('/collection' +  ' .grid', {choice: choiceValue, period:periodValue}, function(){
+            $('.grid').imagesLoaded(function () {
+                $('.grid').masonry({
+                    itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+                    percentPosition: true
+                });
+            });
+        });
+    });
+
+
+    $('.grid').imagesLoaded(function () {
+        $('.grid').masonry({
+            itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+            percentPosition: true
+        });
+    });
+
+
 });
